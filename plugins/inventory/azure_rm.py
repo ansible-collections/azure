e1d5745cdc9294241e645e82a33a0d3b7763d24a
Fred-sun <xiuxi.sun@qq.com>
2020-02-18 12:14:19 +0800

    update new-01

--- plugins/inventory/azure_rm.py
+++ plugins/inventory/azure_rm.py
@@ -178,7 +178,7 @@ from collections import namedtuple
 from ansible import release
 from ansible.plugins.inventory import BaseInventoryPlugin, Constructable
 from ansible.module_utils.six import iteritems
-from ansible_collections.azure.azcollection.plugins.module_utils.azure_rm_common import AzureRMAuth
+from ansible.module_utils.azure_rm_common import AzureRMAuth
 from ansible.errors import AnsibleParserError, AnsibleError
 from ansible.module_utils.parsing.convert_bool import boolean
 from ansible.module_utils._text import to_native, to_bytes
