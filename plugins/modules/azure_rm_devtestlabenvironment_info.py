234d9fc70097eb4481fe98c05580c9850c431798
haiyuan_zhang <haiyuan.zhang1982@gmail.com>
2020-03-19 07:24:05 +0800

    add test group10 (#65)
    *  add integration test cases
    
    Co-authored-by: haiyuazhang <haiyuan.zhang1982@gmaile.com>

--- plugins/modules/azure_rm_devtestlabenvironment_info.py
+++ plugins/modules/azure_rm_devtestlabenvironment_info.py
@@ -122,7 +122,7 @@ environments:
             sample: "{ 'MyTag': 'MyValue' }"
 '''
 
-from ansible.module_utils.azure_rm_common import AzureRMModuleBase
+from ansible_collections.azure.azcollection.plugins.module_utils.azure_rm_common import AzureRMModuleBase
 
 try:
     from msrestazure.azure_exceptions import CloudError
--- plugins/modules/azure_rm_routetable_info.py
+++ plugins/modules/azure_rm_routetable_info.py
@@ -114,7 +114,7 @@ except Exception:
     # This is handled in azure_rm_common
     pass
 
-from ansible.module_utils.azure_rm_common import AzureRMModuleBase, azure_id_to_dict
+from ansible_collections.azure.azcollection.plugins.module_utils.azure_rm_common import AzureRMModuleBase, azure_id_to_dict
 from ansible.module_utils.common.dict_transformations import _camel_to_snake
 
 
