799092978faf8387fa5108f69149fba9271382d7
Fred-sun <xiuxi.sun@qq.com>
2020-05-20 17:30:45 +0800

    small change

--- plugins/modules/azure_rm_virtualmachine.py
+++ plugins/modules/azure_rm_virtualmachine.py
@@ -175,7 +175,7 @@ options:
             - Windows
             - Linux
         default: Linux
-    ephemeral-os-disk:
+    ephemeral_os_disk:
         description:
             - Parameters of ephemeral disk settings that can be specified for operating system disk.
             - Ephemeral OS disk is only supported for VMS Instances using Managed Disk.
