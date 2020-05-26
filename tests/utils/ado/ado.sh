6c73a950c7aa3de8351e55552b9ef37fa5cdc6c0
Fred-sun <xiuxi.sun@qq.com>
2020-05-26 11:16:09 +0800

    update new

--- tests/utils/ado/ado.sh
+++ tests/utils/ado/ado.sh
@@ -19,7 +19,8 @@ export PYTHONIOENCODING="UTF-8"
 export LC_ALL="en_US.utf-8"
 
 pip install virtualenv
-virtualenv --python /usr/bin/python2.7 ~/ansible-venv
+virtualenv --python /usr/bin/python3.7 ~/ansible-venv
+#virtualenv --python /usr/bin/python2.7 ~/ansible-venv
 set +ux
 . ~/ansible-venv/bin/activate
 set -ux
