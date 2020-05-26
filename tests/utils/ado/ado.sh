75b7d4c0209da0a33e5f05a1a59e8895344a2ee6
Fred-sun <xiuxi.sun@qq.com>
2020-05-26 11:19:45 +0800

    change 001

--- tests/utils/ado/ado.sh
+++ tests/utils/ado/ado.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-set -o pipefail -eux
+set -o pip3efail -eux
 
 declare -a args
 IFS='/:' read -ra args <<< "$1"
@@ -10,29 +10,29 @@ group="${args[0]}"
 command -v python
 python -V
 
-command -v pip
-pip --version
-pip list --disable-pip-version-check
+command -v pip3
+pip3 --version
+pip3 list --disable-pip3-version-check
 
 export PATH="${PWD}/bin:${PATH}"
 export PYTHONIOENCODING="UTF-8"
 export LC_ALL="en_US.utf-8"
 
-pip install virtualenv
+pip3 install virtualenv
 virtualenv --python /usr/bin/python3.7 ~/ansible-venv
 #virtualenv --python /usr/bin/python2.7 ~/ansible-venv
 set +ux
 . ~/ansible-venv/bin/activate
 set -ux
 
-pip install ansible==2.9.0 --disable-pip-version-check
+pip3 install ansible==2.9.0 --disable-pip3-version-check
 
 TEST_DIR="${HOME}/.ansible/ansible_collections/azure/azcollection"
 mkdir -p "${TEST_DIR}"
 cp -aT "${SHIPPABLE_BUILD_DIR}" "${TEST_DIR}"
 cd "${TEST_DIR}"
 mkdir -p shippable/testresults
-pip install  -I -r "${TEST_DIR}/requirements-azure.txt"
+pip3 install  -I -r "${TEST_DIR}/requirements-azure.txt"
 
 timeout=60
 
