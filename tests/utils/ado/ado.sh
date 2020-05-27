eb45bff73493e7e9b4b6b3bee49c1d05dcf15e69
Fred-sun <xiuxi.sun@qq.com>
2020-05-27 13:35:53 +0800

    add new

--- tests/utils/ado/ado.sh
+++ tests/utils/ado/ado.sh
@@ -10,28 +10,51 @@ group="${args[0]}"
 command -v python
 python -V
 
-command -v pip
-pip --version
-pip list --disable-pip-version-check
-
-export PATH="${PWD}/bin:${PATH}"
-export PYTHONIOENCODING="UTF-8"
-export LC_ALL="en_US.utf-8"
-
-pip install virtualenv
-virtualenv --python /usr/bin/python$(python_ver) ~/ansible-venv
-set +ux
-. ~/ansible-venv/bin/activate
-set -ux
-
-pip install ansible==$(ansible_ver) --disable-pip-version-check
-
-TEST_DIR="${HOME}/.ansible/ansible_collections/azure/azcollection"
-mkdir -p "${TEST_DIR}"
-cp -aT "${SHIPPABLE_BUILD_DIR}" "${TEST_DIR}"
-cd "${TEST_DIR}"
-mkdir -p shippable/testresults
-pip install  -I -r "${TEST_DIR}/requirements-azure.txt"
+if $(python_ver)=='2.7':
+    command -v pip
+    pip --version
+    pip list --disable-pip-version-check
+    
+    export PATH="${PWD}/bin:${PATH}"
+    export PYTHONIOENCODING="UTF-8"
+    export LC_ALL="en_US.utf-8"
+    
+    pip install virtualenv
+    virtualenv --python /usr/bin/python$(python_ver) ~/ansible-venv
+    set +ux
+    . ~/ansible-venv/bin/activate
+    set -ux
+    
+    pip install ansible==$(ansible_ver) --disable-pip-version-check
+    
+    TEST_DIR="${HOME}/.ansible/ansible_collections/azure/azcollection"
+    mkdir -p "${TEST_DIR}"
+    cp -aT "${SHIPPABLE_BUILD_DIR}" "${TEST_DIR}"
+    cd "${TEST_DIR}"
+    mkdir -p shippable/testresults
+    pip install  -I -r "${TEST_DIR}/requirements-azure.txt"
+else:
+    command -v pip3
+    pip3 --version
+    pip3 list --disable-pip-version-check
+    
+    export PATH="${PWD}/bin:${PATH}"
+    export PYTHONIOENCODING="UTF-8"
+    export LC_ALL="en_US.utf-8"
+    
+    pip3 install virtualenv
+    virtualenv --python /usr/bin/python$(python_ver) ~/ansible-venv
+    set +ux
+    . ~/ansible-venv/bin/activate
+    set -ux
+    
+    pip3 install ansible==$(ansible_ver) --disable-pip-version-check
+    
+    TEST_DIR="${HOME}/.ansible/ansible_collections/azure/azcollection"
+    mkdir -p "${TEST_DIR}"
+    cp -aT "${SHIPPABLE_BUILD_DIR}" "${TEST_DIR}"
+    cd "${TEST_DIR}"
+    mkdir -p shippable/testresults
 
 timeout=60
 
