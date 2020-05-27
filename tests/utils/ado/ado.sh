6c39453552f530b8f4251f1afabf3bf889314fb1
Fred-sun <xiuxi.sun@qq.com>
2020-05-27 15:13:56 +0800

    add new

--- tests/utils/ado/ado.sh
+++ tests/utils/ado/ado.sh
@@ -9,29 +9,54 @@ group="${args[0]}"
 
 command -v python
 python -V
-
-command -v pip
-pip --version
-pip list --disable-pip-version-check
-
-export PATH="${PWD}/bin:${PATH}"
-export PYTHONIOENCODING="UTF-8"
-export LC_ALL="en_US.utf-8"
-
-pip install virtualenv
-virtualenv --python /usr/bin/python2.7 ~/ansible-venv
-set +ux
-. ~/ansible-venv/bin/activate
-set -ux
-
-pip install ansible==2.9.0 --disable-pip-version-check
-
-TEST_DIR="${HOME}/.ansible/ansible_collections/azure/azcollection"
-mkdir -p "${TEST_DIR}"
-cp -aT "${SHIPPABLE_BUILD_DIR}" "${TEST_DIR}"
-cd "${TEST_DIR}"
-mkdir -p shippable/testresults
-pip install  -I -r "${TEST_DIR}/requirements-azure.txt"
+if [ "$(PYTHON_VER)"==2.7]
+then
+    command -v pip
+    pip --version
+    pip list --disable-pip-version-check
+    
+    export PATH="${PWD}/bin:${PATH}"
+    export PYTHONIOENCODING="UTF-8"
+    export LC_ALL="en_US.utf-8"
+    
+    pip install virtualenv
+    virtualenv --python /usr/bin/python2.7 ~/ansible-venv
+    set +ux
+    . ~/ansible-venv/bin/activate
+    set -ux
+    
+    pip install ansible==2.9.0 --disable-pip-version-check
+    
+    TEST_DIR="${HOME}/.ansible/ansible_collections/azure/azcollection"
+    mkdir -p "${TEST_DIR}"
+    cp -aT "${SHIPPABLE_BUILD_DIR}" "${TEST_DIR}"
+    cd "${TEST_DIR}"
+    mkdir -p shippable/testresults
+    pip install  -I -r "${TEST_DIR}/requirements-azure.txt"
+else      
+    command -v pip3
+    pip3 --version
+    pip3 list --disable-pip-version-check
+    
+    export PATH="${PWD}/bin:${PATH}"
+    export PYTHONIOENCODING="UTF-8"
+    export LC_ALL="en_US.utf-8"
+    
+    pip3 install virtualenv
+    virtualenv --python "/usr/bin/python$(PYTHON_VER)" ~/ansible-venv
+    set +ux
+    . ~/ansible-venv/bin/activate
+    set -ux
+    
+    pip3 install ansible==$(ANSIBLE_VER) --disable-pip-version-check
+    
+    TEST_DIR="${HOME}/.ansible/ansible_collections/azure/azcollection"
+    mkdir -p "${TEST_DIR}"
+    cp -aT "${SHIPPABLE_BUILD_DIR}" "${TEST_DIR}"
+    cd "${TEST_DIR}"
+    mkdir -p shippable/testresults
+    pip3 install  -I -r "${TEST_DIR}/requirements-azure.txt"
+fi
 
 timeout=60
 
