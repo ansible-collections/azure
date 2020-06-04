d318cec57db10f3ecc6bfc5a602e41e7c2240e7d
Fred-sun <xiuxi.sun@qq.com>
2020-06-04 15:49:55 +0800

    add new ---5

--- plugins/modules/azure_rm_networkinterface.py
+++ plugins/modules/azure_rm_networkinterface.py
@@ -463,10 +463,10 @@ except ImportError:
     pass
 
 from ansible_collections.azure.azcollection.plugins.module_utils.azure_rm_common import (AzureRMModuleBase,
-                                                                                        azure_id_to_dict,
-                                                                                        normalize_location_name,
-                                                                                        format_resource_id
-                                                                                        )
+                                                                                         azure_id_to_dict,
+                                                                                         normalize_location_name,
+                                                                                         format_resource_id
+                                                                                         )
 from ansible.module_utils._text import to_native
 
 
