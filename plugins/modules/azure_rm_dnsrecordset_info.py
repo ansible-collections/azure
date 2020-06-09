fa055742a7d9732dc9c23782f28465c06bde7a64
Fred-sun <xiuxi.sun@qq.com>
2020-06-09 15:04:47 +0800

    update

--- plugins/modules/azure_rm_dnsrecordset_info.py
+++ plugins/modules/azure_rm_dnsrecordset_info.py
@@ -193,7 +193,7 @@ class AzureRMRecordSetInfo(AzureRMModuleBase):
 
         is_old_facts = self.module._name == 'azure_rm_dnsrecordset_facts'
         if is_old_facts:
-            self.module.deprecate("The 'azure_rm_dnsrecordset_facts' module has been renamed to 'azure_rm_dnsrecordset_info'", version='2.13')
+            self.module.deprecate("The 'azure_rm_dnsrecordset_facts' module has been renamed to 'azure_rm_dnsrecordset_info'", version=(2, 9))
 
         for key in self.module_arg_spec:
             setattr(self, key, kwargs[key])
