84beca69ff63d0ef147177fddc54767340678f83
Fred-sun <xiuxi.sun@qq.com>
2020-07-16 15:19:15 +0800

    add new

--- tests/utils/sendmail/sendmail.py
+++ tests/utils/sendmail/sendmail.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python
+#
+# Copyright (c) 2020 Fred-sun, <v-xisu@microsoft.com>
+#
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+from __future__ import absolute_import, division, print_function
 import smtplib
 import sys
 from email.mime.text import MIMEText
