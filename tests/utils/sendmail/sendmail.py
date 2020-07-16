5d371d5e1f8ae00fc19e899a9e8d13bcce8a1901
Fred-sun <xiuxi.sun@qq.com>
2020-07-16 14:30:08 +0800

    add new

--- tests/utils/sendmail/sendmail.py
+++ tests/utils/sendmail/sendmail.py
@@ -5,18 +5,17 @@ from email.header import Header
 
 from_addr = sys.argv[1]
 password = sys.argv[2]
-print(sys.argv)
-to_addr = [ 'xiuxi.sun@qq.com']
-    
+to_addr = ['xiuxi.sun@qq.com',]
+
 smtp_server = 'smtp.qq.com'
  
-msg = MIMEText('DEV ver pipeline test fail，detail info as Link：*******','plain','utf-8')
-     
+msg = MIMEText('DEV ver pipeline test fail，detail info as Link：*******' ,'plain', 'utf-8')
+
 msg['From'] = Header(from_addr)
 msg['Subject'] = Header('ansible-collection DEV TEST')
-    
+
 server = smtplib.SMTP_SSL()
-server.connect(smtp_server,465)
+server.connect(smtp_server, 465)
 server.login(from_addr, password)
 for mail_box in to_addr:
     msg['To'] = Header(mail_box)
