664ad63dcaddd3c59544d4a37e2fc24ab186bab9
Leonardo Malik <leonardo.malik@paynovate.com>
2020-07-21 09:27:50 +0200

    Make ip_configurations primary option required

--- plugins/modules/azure_rm_networkinterface.py
+++ plugins/modules/azure_rm_networkinterface.py
@@ -533,7 +533,7 @@ ip_configuration_spec = dict(
     public_ip_address_name=dict(type='str', aliases=['public_ip_address', 'public_ip_name']),
     public_ip_allocation_method=dict(type='str', choices=['Dynamic', 'Static'], default='Dynamic'),
     load_balancer_backend_address_pools=dict(type='list'),
-    primary=dict(type='bool', default=False),
+    primary=dict(type='bool', required=True),
     application_security_groups=dict(type='list', elements='raw')
 )
 
