381dbf9c29d4fad3adc32737dd3b94ad80e8db55
Fred-sun <xiuxi.sun@qq.com>
2020-09-09 16:17:18 +0800

    format line style

--- tests/utils/ado/ado.sh
+++ tests/utils/ado/ado.sh
@@ -32,11 +32,11 @@ then
 else
     if [ "$2" = "3.8" ]
     then
-	apt update
-	apt install software-properties-common
-	add-apt-repository ppa:deadsnakes/ppa
-	apt install python"$2" -y
-	update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.8 1
+        apt update
+        apt install software-properties-common
+        add-apt-repository ppa:deadsnakes/ppa
+        apt install python"$2" -y
+        update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.8 1
 
     pip3 install virtualenv
     virtualenv --python /usr/bin/python"$2" ~/ansible-venv
