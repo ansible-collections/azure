39f525d21342edb21d205d638624344b4fd49d44
Fred-sun <xiuxi.sun@qq.com>
2020-09-09 16:05:54 +0800

    update pr-pipeline python version

--- tests/utils/ado/ado.sh
+++ tests/utils/ado/ado.sh
@@ -51,6 +51,14 @@ then
 	pip install paramiko PyYAML Jinja2  httplib2 six
     fi
 else
+    if [ "$2" = "3.8" ]
+    then
+	apt update
+	apt install software-properties-common
+	add-apt-repository ppa:deadsnakes/ppa
+	apt install python3.8 -y
+	update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.8 1
+
     if [ "$3" = "devel" ]
     then
         pip3 install git+https://github.com/ansible/ansible.git@devel  --disable-pip-version-check
