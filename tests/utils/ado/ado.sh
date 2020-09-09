d1da7569a3086c813001b83a49e9141a78b14566
Fred-sun <xiuxi.sun@qq.com>
2020-09-09 16:13:18 +0800

    update new

--- tests/utils/ado/ado.sh
+++ tests/utils/ado/ado.sh
@@ -30,6 +30,14 @@ then
     pip install virtualenv
     virtualenv --python /usr/bin/python2.7 ~/ansible-venv
 else
+    if [ "$2" = "3.8" ]
+    then
+	apt update
+	apt install software-properties-common
+	add-apt-repository ppa:deadsnakes/ppa
+	apt install python"$2" -y
+	update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.8 1
+
     pip3 install virtualenv
     virtualenv --python /usr/bin/python"$2" ~/ansible-venv
 fi
@@ -51,14 +59,6 @@ then
 	pip install paramiko PyYAML Jinja2  httplib2 six
     fi
 else
-    if [ "$2" = "3.8" ]
-    then
-	apt update
-	apt install software-properties-common
-	add-apt-repository ppa:deadsnakes/ppa
-	apt install python3.8 -y
-	update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.8 1
-
     if [ "$3" = "devel" ]
     then
         pip3 install git+https://github.com/ansible/ansible.git@devel  --disable-pip-version-check
