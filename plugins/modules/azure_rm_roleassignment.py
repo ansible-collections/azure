383c77015504f6c28c478a429e656e366591ab2a
Fred-sun <xiuxi.sun@qq.com>
2020-10-15 15:44:00 +0800

    update new

--- plugins/modules/azure_rm_roleassignment.py
+++ plugins/modules/azure_rm_roleassignment.py
@@ -26,7 +26,7 @@ options:
         description:
             - Unique name of role assignment.
             - The role assignment name must be a GUID, sample as "3ce0cbb0-58c4-4e6d-a16d-99d86a78b3ca".
-        require: true
+        required: True
     assignee_object_id:
         description:
             - The object id of assignee. This maps to the ID inside the Active Directory.
@@ -84,8 +84,6 @@ id:
     sample: "/subscriptions/xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx/providers/Microsoft.Authorization/roleAssignments/xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
 '''
 
-import uuid
-
 try:
     from ansible_collections.azure.azcollection.plugins.module_utils.azure_rm_common import AzureRMModuleBase
     from msrestazure.azure_exceptions import CloudError
@@ -117,7 +115,7 @@ class AzureRMRoleAssignment(AzureRMModuleBase):
         self.module_arg_spec = dict(
             name=dict(
                 type='str',
-                require='true'
+                required=True
             ),
             scope=dict(
                 type='str'
