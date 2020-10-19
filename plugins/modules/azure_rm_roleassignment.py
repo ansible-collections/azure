779859ad401f372f20afa07be0410d19fbdbb499
Paul Aiton <paul@aiton.info>
2020-10-18 22:23:33 -0700

    it's working

--- plugins/modules/azure_rm_roleassignment.py
+++ plugins/modules/azure_rm_roleassignment.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 #
+# Copyright (c) 2020 Paul Aiton, (@paultaiton)
 # Copyright (c) 2018 Yunge Zhu, (@yungezz)
 #
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
@@ -56,6 +57,7 @@ extends_documentation_fragment:
 
 author:
     - Yunge Zhu(@yungezz)
+    - Paul Aiton(@paultaiton)
 
 '''
 
@@ -100,30 +102,38 @@ class AzureRMRoleAssignment(AzureRMModuleBase):
 
     def __init__(self):
         self.module_arg_spec = dict(
-            name=dict(type='str'),
-            scope=dict(type='str'),
             assignee_object_id=dict(type='str'),
+            id=dict(type='str'),
+            name=dict(type='str'),
             role_definition_id=dict(type='str'),
-            role_assignment_id=dict(type='str'),
+            scope=dict(type='str'),
             state=dict(type='str', default='present', choices=['present', 'absent'])
         )
 
-        self.name = None
-        self.scope = None
         self.assignee_object_id = None
+        self.id = None
+        self.name = None
         self.role_definition_id = None
+        self.scope = None
+        self.state = None
 
         self.results = dict(
             changed=False,
             id=None,
         )
-        self.state = None
 
-        mutually_exclusive = [['name', 'role_assignment_id'], ['scope', 'role_assignment_id']]
+        mutually_exclusive = [['name', 'id'], ['scope', 'id']]
+        required_one_of = [['scope', 'id']]
+        required_if = [
+            ["state", "present", ["assignee_object_id", "role_definition_id"]]
+        ]
 
         super(AzureRMRoleAssignment, self).__init__(derived_arg_spec=self.module_arg_spec,
                                                     supports_check_mode=True,
-                                                    supports_tags=False)
+                                                    supports_tags=False,
+                                                    required_one_of=required_one_of,
+                                                    required_if=required_if,
+                                                    mutually_exclusive=mutually_exclusive)
 
     def exec_module(self, **kwargs):
         """Main module execution method"""
@@ -133,23 +143,14 @@ class AzureRMRoleAssignment(AzureRMModuleBase):
 
         if self.name and not self.scope:
             self.fail("Parameter Error: setting name requires a scope to also be set.")
-        if not self.scope and not self.role_assignment_id:
-            self.fail("Parameter Error: either a scope or an assignment_id must be set.")
 
         existing_assignment = None
         response = None
 
-        # TODO get existing assignment
         existing_assignment = self.get_roleassignment()
 
         if existing_assignment:
-            self.results['id'] = existing_assignment.get('id')
-            self.results['name'] = existing_assignment.get('name')
-            self.results['principal_id'] = existing_assignment.get('principal_id')
-            self.results['principal_type'] = existing_assignment.get('principal_type')
-            self.results['role_definition_id'] = existing_assignment.get('role_definition_id')
-            self.results['scope'] = existing_assignment.get('scope')
-            self.results['type'] = existing_assignment.get('assignment')
+            self.set_results(existing_assignment)
 
         if self.state == 'present':
             # check if the role assignment exists
@@ -161,7 +162,7 @@ class AzureRMRoleAssignment(AzureRMModuleBase):
                 if self.check_mode:
                     return self.results
                 response = self.create_roleassignment()
-                self.results['id'] = response['id']
+                self.set_results(response)
 
             else:
                 self.log("Role assignment already exists, not updatable")
@@ -180,7 +181,8 @@ class AzureRMRoleAssignment(AzureRMModuleBase):
                 self.log('role assignment deleted')
 
             else:
-                self.fail("role assignment {0} not exists.".format(self.name))
+                # If assignment doesn't exist, that's the desired state.
+                self.log("role assignment {0} does not exist.".format(self.name))
 
         return self.results
 
@@ -192,11 +194,17 @@ class AzureRMRoleAssignment(AzureRMModuleBase):
         '''
         self.log("Creating role assignment {0}".format(self.name))
 
+        response = None
         try:
-            parameters = self.assignment_models.RoleAssignmentCreateParameters(role_definition_id=self.role_definition_id, principal_id=self.assignee_object_id)
-            response = self.assignment_client.role_assignments.create(scope=self.scope,
-                                                                      role_assignment_name=self.name,
-                                                                      parameters=parameters)
+            parameters = self.authorization_models.RoleAssignmentCreateParameters(role_definition_id=self.role_definition_id, principal_id=self.assignee_object_id)
+            if self.id:
+                response = self.authorization_client.role_assignments.create_by_id(role_id=self.id, parameters=parameters)
+            elif self.scope:
+                if not self.name:
+                    self.name = str(uuid.uuid4())
+                response = self.authorization_client.role_assignments.create(scope=self.scope,
+                                                                             role_assignment_name=self.name,
+                                                                             parameters=parameters)
 
         except CloudError as exc:
             self.log('Error attempting to create role assignment.')
@@ -211,7 +219,7 @@ class AzureRMRoleAssignment(AzureRMModuleBase):
         '''
         self.log("Deleting the role assignment {0}".format(self.name))
         try:
-            response = self.assignment_client.role_assignments.delete_by_id(role_id=assignment_id)
+            response = self.authorization_client.role_assignments.delete_by_id(role_id=assignment_id)
         except CloudError as e:
             self.log('Error attempting to delete the role assignment.')
             self.fail("Error deleting the role assignment: {0}".format(str(e)))
@@ -228,32 +236,63 @@ class AzureRMRoleAssignment(AzureRMModuleBase):
 
         role_assignment = None
 
-        try:
-            response = self.authorization_client.role_assignments.list()
+        if self.id:
+            try:
+                response = self.authorization_client.role_assignments.get_by_id(role_id=self.id)
+                role_assignment = self.roleassignment_to_dict(response)
+                if role_assignment and self.assignee_object_id and role_assignment.get('assignee_object_id') != self.assignee_object_id:
+                    self.fail('State Mismatch Error: The assignment ID exists, but does not match the provided assignee.')
 
-            if self.name:
-                response = [role_assignment for role_assignment in response if role_assignment.get('name') == self.name]
-            if self.scope:
-                response = [role_assignment for role_assignment in response if role_assignment.get('scope') == self.scope]
-            if self.assignee_object_id:
-                response = [role_assignment for role_assignment in response if role_assignment.get('assignee_object_id') == self.assignee_object_id]
-            if self.role_definition_id:
-                response = [role_assignment for role_assignment in response if role_assignment.get('role_definition_id') == self.role_definition_id]
-            if self.id:
-                response = [role_assignment for role_assignment in response if role_assignment.get('id') == self.id]
-            if response:
-                role_assignment = self.roleassignment_to_dict(response[0])
+                if role_assignment and self.role_definition_id and role_assignment.get('role_definition_id') != self.role_definition_id:
+                    self.fail('State Mismatch Error: The assignment ID exists, but does not match the provided role.')
 
-        except CloudError as ex:
-            self.log("Error when fetching state from Azure.")
+            except CloudError as ex:
+                self.log("Didn't find role assignments id {0}".format(self.id))
+
+        elif self.name and self.scope:
+            try:
+                response = self.authorization_client.role_assignments.get(scope=self.scope, role_assignment_name=self.name)
+                role_assignment = self.roleassignment_to_dict(response)
+                if role_assignment and self.assignee_object_id and role_assignment.get('assignee_object_id') != self.assignee_object_id:
+                    self.fail('State Mismatch Error: The assignment name exists, but does not match the provided assignee.')
+
+                if role_assignment and self.role_definition_id and role_assignment.get('role_definition_id') != self.role_definition_id:
+                    self.fail('State Mismatch Error: The assignment name exists, but does not match the provided role.')
+
+            except CloudError as ex:
+                self.log("Didn't find role assignment by name {0} at scope {1}".format(self.name, self.scope))
+
+        else:
+            try:
+                response = list(self.authorization_client.role_assignments.list())
+                response = [self.roleassignment_to_dict(role_assignment) for role_assignment in response]
+                if self.scope and self.assignee_object_id and self.role_definition_id:
+                    response = [role_assignment for role_assignment in response if role_assignment.get('scope') == self.scope]
+                    response = [role_assignment for role_assignment in response if role_assignment.get('assignee_object_id') == self.assignee_object_id]
+                    response = [role_assignment for role_assignment in response if role_assignment.get('role_definition_id') == self.role_definition_id]
+                else:
+                    self.fail('If id or name are not supplied, then assignee_object_id and role_definition_id are required.')
+                if response:
+                    role_assignment = response[0]
+            except CloudError as ex:
+                self.log("Didn't find role assignments for subscription {0}".format(self.subscription_id))
 
         return role_assignment
 
+    def set_results(self, assignment):
+        self.results['id'] = assignment.get('id')
+        self.results['name'] = assignment.get('name')
+        self.results['type'] = assignment.get('type')
+        self.results['assignee_object_id'] = assignment.get('assignee_object_id')
+        self.results['principal_type'] = assignment.get('principal_type')
+        self.results['role_definition_id'] = assignment.get('role_definition_id')
+        self.results['scope'] = assignment.get('scope')
+
     def roleassignment_to_dict(self, assignment):
         return dict(
+            assignee_object_id=assignment.principal_id,
             id=assignment.id,
             name=assignment.name,
-            principal_id=assignment.principal_id,
             principal_type=assignment.principal_type,
             role_definition_id=assignment.role_definition_id,
             scope=assignment.scope,
