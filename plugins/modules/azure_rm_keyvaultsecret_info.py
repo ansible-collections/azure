e2c8ceea1ed0a2365a1e7bfcd9352b2c05a97a0a
Fred-sun <xiuxi.sun@qq.com>
2021-01-11 16:22:37 +0800

    add new

--- plugins/modules/azure_rm_keyvaultsecret_info.py
+++ plugins/modules/azure_rm_keyvaultsecret_info.py
@@ -187,8 +187,8 @@ def secretbundle_to_dict(bundle):
                     recovery_level=bundle.attributes.recovery_level),
                 sid=bundle.id,
                 version=KeyVaultId.parse_secret_id(bundle.id).version,
-                type=bundle.content_type,
-                dd=bundle,
+                type=bundle,
+                #type=bundle.content_type,
                 secret=bundle.value)
 
 
