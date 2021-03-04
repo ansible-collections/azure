25313209784a9f5082dba3a1d2171ca22973bdcb
Fred-sun <37327967+Fred-sun@users.noreply.github.com>
2021-03-04 14:24:15 +0800

    Add tags for azure_rm_loganalyticsworkspace (#434)
    * add tags for azure_rm_loganalyticsworkspace
    
    * Add update function for this module

--- plugins/modules/azure_rm_loganalyticsworkspace.py
+++ plugins/modules/azure_rm_loganalyticsworkspace.py
@@ -214,8 +214,22 @@ class AzureRMLogAnalyticsWorkspace(AzureRMModuleBase):
             changed = True
             workspace = self.log_analytics_models.Workspace(sku=self.log_analytics_models.WorkspaceSku(name=self.sku),
                                                             retention_in_days=self.retention_in_days,
-                                                            location=self.location)
+                                                            location=self.location,
+                                                            tags=self.tags)
             if not self.check_mode:
+                workspace = self.create_workspace(workspace)
+        elif workspace and self.state == 'present':
+            if workspace.retention_in_days != self.retention_in_days:
+                changed = True
+            results = dict()
+            update_tags, results['tags'] = self.update_tags(workspace.tags)
+            if update_tags:
+                changed = True
+            if not self.check_mode and changed:
+                workspace = self.log_analytics_models.Workspace(sku=self.log_analytics_models.WorkspaceSku(name=self.sku),
+                                                                retention_in_days=self.retention_in_days,
+                                                                location=self.location,
+                                                                tags=results['tags'])
                 workspace = self.create_workspace(workspace)
         elif workspace and self.state == 'absent':
             changed = True
