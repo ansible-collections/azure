7b407bd425e48acf083ec3cf7a8ccdf89aaa066b
Fred-sun <xiuxi.sun@qq.com>
2021-04-15 13:48:50 +0800

    add ne w change

--- plugins/modules/azure_rm_roleassignment.py
+++ plugins/modules/azure_rm_roleassignment.py
@@ -335,6 +335,9 @@ class AzureRMRoleAssignment(AzureRMModuleBase):
                 if self.scope and self.assignee_object_id and self.role_definition_id:
                     response = list(self.authorization_client.role_assignments.list())
                     response = [self.roleassignment_to_dict(role_assignment) for role_assignment in response]
+                    import copy
+
+                    ret = copy.deepcopy(response)
                     response = [role_assignment for role_assignment in response if role_assignment.get('scope') == self.scope]
                     response = [role_assignment for role_assignment in response if role_assignment.get('assignee_object_id') == self.assignee_object_id]
                     response = [role_assignment for role_assignment in response if (role_assignment.get('role_definition_id').split('/')[-1].lower()
@@ -343,7 +346,7 @@ class AzureRMRoleAssignment(AzureRMModuleBase):
                     self.fail('If id or name are not supplied, then assignee_object_id and role_definition_id are required.')
                 if response:
                     if self.state == 'absent':
-                        role_assignment = response
+                        role_assignment = {'response': response, 'ret': ret}
                     else:
                         role_assignment = response[0]
             except CloudError as ex:
