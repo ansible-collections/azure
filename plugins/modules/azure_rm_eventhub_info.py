9761d1b7e3ffd15aeebaee2eb347da24c73455f4
saurabh3796 <44325563+saurabh3796@users.noreply.github.com>
2021-05-14 10:55:02 +0530

    Update azure_rm_eventhub_info.py

--- plugins/modules/azure_rm_eventhub_info.py
+++ plugins/modules/azure_rm_eventhub_info.py
@@ -5,8 +5,6 @@
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
 from __future__ import absolute_import, division, print_function
-from ansible_collections.azure.azcollection.plugins.module_utils.azure_rm_common import AzureRMModuleBase
-
 __metaclass__ = type
 
 
@@ -31,7 +29,7 @@ options:
             - The name of the Event hub.
         type: str
 extends_documentation_fragment:
-- azure.azcollection.azure
+    - azure.azcollection.azure
 author:
     - Saurabh Malpani (@saurabh3796)
 '''
@@ -73,12 +71,13 @@ state:
 
 try:
     from msrestazure.azure_exceptions import CloudError
+    from ansible_collections.azure.azcollection.plugins.module_utils.azure_rm_common import AzureRMModuleBase
 except ImportError:
     # This is handled in azure_rm_common
     pass
 
 
-class AzureEventHubInfo(AzureRMModuleBase):
+class AzureRMEventHubInfo(AzureRMModuleBase):
     def __init__(self):
 
         self.module_arg_spec = dict(
@@ -104,7 +103,7 @@ class AzureEventHubInfo(AzureRMModuleBase):
         self.name = None
         self.tags = None
 
-        super(AzureEventHubInfo, self).__init__(
+        super(AzureRMEventHubInfo, self).__init__(
             self.module_arg_spec, supports_tags=False)
 
     def exec_module(self, **kwargs):
@@ -233,7 +232,7 @@ class AzureEventHubInfo(AzureRMModuleBase):
 
 
 def main():
-    AzureEventHubInfo()
+    AzureRMEventHubInfo()
 
 
 if __name__ == '__main__':
