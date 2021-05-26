12ea22597d55298852e305496ffa194fdc9fa0a4
saurabh3796 <44325563+saurabh3796@users.noreply.github.com>
2021-05-26 18:20:59 +0530

    Update azure_rm_eventhub.py

--- plugins/modules/azure_rm_eventhub.py
+++ plugins/modules/azure_rm_eventhub.py
@@ -158,12 +158,12 @@ state:
 
 try:
     from msrestazure.azure_exceptions import CloudError
+    from azure.mgmt.eventhub.models import Eventhub, EHNamespace
+    from azure.mgmt.eventhub.models.sku import Sku
 except ImportError:
     # This is handled in azure_rm_common
     pass
 from ansible_collections.azure.azcollection.plugins.module_utils.azure_rm_common import AzureRMModuleBase
-from azure.mgmt.eventhub.models import Eventhub, EHNamespace
-from azure.mgmt.eventhub.models.sku import Sku
 import time
 
 
