b7e79a67501ba2b772a4920993efce76e70dde46
xuzhang3 <xuzhang3@microsoft.com>
2021-08-20 16:45:12 +0800

    format

--- plugins/modules/azure_rm_virtualmachine.py
+++ plugins/modules/azure_rm_virtualmachine.py
@@ -1980,11 +1980,11 @@ class AzureRMVirtualMachine(AzureRMModuleBase):
                                                                        orderby='name desc')
         except Exception as exc:
             self.fail("Error fetching image {0} {1} {2} {3} {4} - {5}".format(self.image['publisher'],
-                                                                      self.image['offer'],
-                                                                      self.image['sku'],
-                                                                      "top=1",
-                                                                      "orderby=name desc",
-                                                                      str(exc)))
+                                                                              self.image['offer'],
+                                                                              self.image['sku'],
+                                                                              "top=1",
+                                                                              "orderby=name desc",
+                                                                              str(exc)))
         if versions and len(versions) > 0:
             if self.image['version'] == 'latest':
                 return versions[len(versions) - 1]
