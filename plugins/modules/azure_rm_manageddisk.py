c8b2ddfc581d06c367306b6152da7bfca9b80df9
Maxence <max@23.tf>
2021-08-27 21:18:01 +0000

    Fix os_type comparison in azure_rm_manageddisk with existing disk

--- plugins/modules/azure_rm_manageddisk.py
+++ plugins/modules/azure_rm_manageddisk.py
@@ -442,7 +442,7 @@ class AzureRMManagedDisk(AzureRMModuleBase):
             if not found_disk['disk_size_gb'] == new_disk['disk_size_gb']:
                 resp = True
         if new_disk.get('os_type'):
-            if not found_disk['os_type'] == new_disk['os_type']:
+            if not self.compute_models.OperatingSystemTypes(found_disk['os_type'].capitalize()) == new_disk['os_type']:
                 resp = True
         if new_disk.get('sku'):
             if not found_disk['storage_account_type'] == new_disk['sku'].name:
