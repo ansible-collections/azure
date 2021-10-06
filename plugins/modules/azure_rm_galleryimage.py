8402383406a611b3bb5ea78b4241afda41e75ab3
Justin Trantham <57366553+netgain-justin-trantham@users.noreply.github.com>
2021-10-06 16:47:56 -0500

    Update azure_rm_galleryimage.py
    Added Hyper V Generation

--- plugins/modules/azure_rm_galleryimage.py
+++ plugins/modules/azure_rm_galleryimage.py
@@ -69,6 +69,12 @@ options:
             - specialized
         required: true
         type: str
+    hyperveneration:
+        description:
+            - This property allows you to specify the Hyper V Version of the Virtual Machines.
+        choices:
+            - V1
+            - v2
     end_of_life_date:
         description:
             - The end of life date of the gallery Image Definition.
@@ -266,6 +272,12 @@ class AzureRMGalleryImages(AzureRMModuleBaseExt):
                 disposition='/properties/osState',
                 choices=['generalized',
                          'specialized']
+            ),
+            hypervgeneration=dict(
+                type='str',
+                disposition='/properties/hyperVGeneration',
+                choices=['V1',
+                         'V2']
             ),
             end_of_life_date=dict(
                 type='str',
