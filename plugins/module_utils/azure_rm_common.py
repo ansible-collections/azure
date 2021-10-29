5b50c91800efeeb7d86b876e1c7c5987e0008fdd
Fred-sun <xiuxi.sun@qq.com>
2021-10-29 17:13:24 +0800

    fix small

--- plugins/module_utils/azure_rm_common.py
+++ plugins/module_utils/azure_rm_common.py
@@ -280,7 +280,6 @@ try:
     import azure.mgmt.datalake.store.models as DataLakeStoreAccountModel
     from azure.mgmt.notificationhubs import NotificationHubsManagementClient
     from azure.mgmt.eventhub import EventHubManagementClient
-    from azure.identity._credentials import client_secret
 
 except ImportError as exc:
     Authentication = object
@@ -725,7 +724,7 @@ class AzureRMModuleBase(object):
         self.log("Check to see if public IP {0} exists".format(public_ip_name))
         try:
             pip = self.network_client.public_ip_addresses.get(resource_group, public_ip_name)
-        except Exception:
+        except CloudError:
             pass
 
         if pip:
@@ -765,7 +764,7 @@ class AzureRMModuleBase(object):
         self.log("Check to see if security group {0} exists".format(security_group_name))
         try:
             group = self.network_client.network_security_groups.get(resource_group, security_group_name)
-        except Exception:
+        except CloudError:
             pass
 
         if group:
@@ -876,7 +875,7 @@ class AzureRMModuleBase(object):
 
         return client
 
-    def get_mgmt_svc_client(self, client_type, base_url=None, api_version=None, suppress_subscription_id=False, is_track2=False):
+    def get_mgmt_svc_client(self, client_type, base_url=None, api_version=None, suppress_subscription_id=False):
         self.log('Getting management service client {0}'.format(client_type.__name__))
         self.check_client_version(client_type)
 
@@ -934,8 +933,7 @@ class AzureRMModuleBase(object):
             setattr(client, '_ansible_models', importlib.import_module(client_type.__module__).models)
             client.models = types.MethodType(_ansible_get_models, client)
 
-        if not is_track2:
-            client.config = self.add_user_agent(client.config)
+        client.config = self.add_user_agent(client.config)
 
         if self.azure_auth._cert_validation_mode == 'ignore':
             client.config.session_configuration_callback = self._validation_ignore_callback
@@ -1518,9 +1516,6 @@ class AzureRMAuth(object):
                                                                  cloud_environment=self._cloud_environment,
                                                                  resource=graph_resource if self.is_ad_resource else rm_resource,
                                                                  verify=self._cert_validation_mode == 'validate')
-            self.azure_credential_track2 = client_secret.ClientSecretCredential(client_id=self.credentials['client_id'],
-                                                                                client_secret=self.credentials['secret'],
-                                                                                tenant_id=self.credentials['tenant'])
 
         elif self.credentials.get('ad_user') is not None and \
                 self.credentials.get('password') is not None and \
