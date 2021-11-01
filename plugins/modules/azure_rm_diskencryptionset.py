42c2234a3e5947b1c97df73da6ff27790a200d69
Fred-sun <xiuxi.sun@qq.com>
2021-11-01 20:42:02 +0800

    fix azure_rm_diskencryption test fail

--- plugins/modules/azure_rm_diskencryptionset.py
+++ plugins/modules/azure_rm_diskencryptionset.py
@@ -298,8 +298,7 @@ class AzureRMDiskEncryptionSet(AzureRMModuleBase):
             # delete the disk encryption set
             response = self.compute_client.disk_encryption_sets.begin_delete(resource_group_name=self.resource_group,
                                                                              disk_encryption_set_name=self.name)
-            if isinstance(response, LROPoller):
-                response = self.get_poller_result(response)
+            response = self.get_poller_result(response)
         except Exception as exc:
             self.fail("Error deleting disk encryption set {0} - {1}".format(self.name, str(exc)))
         return response
