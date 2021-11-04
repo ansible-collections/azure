b2e3054232ae06908e781dc20a509bf12859866c
Fred-sun <xiuxi.sun@qq.com>
2021-11-04 14:26:50 +0800

    fix sanity test fail

--- plugins/modules/azure_rm_virtualmachinescalesetinstance.py
+++ plugins/modules/azure_rm_virtualmachinescalesetinstance.py
@@ -226,7 +226,7 @@ class AzureRMVirtualMachineScaleSetInstance(AzureRMModuleBase):
         try:
             poller = self.compute_client.virtual_machine_scale_sets.begin_update_instances(resource_group_name=self.resource_group,
                                                                                            vm_scale_set_name=self.vmss_name,
-                                                                                           vm_instance_i_ds={'instance_ids':instance_id})
+                                                                                           vm_instance_i_ds={'instance_ids': instance_id})
             self.get_poller_result(poller)
         except Exception as exc:
             self.log("Error applying latest model {0} - {1}".format(self.vmss_name, str(exc)))
