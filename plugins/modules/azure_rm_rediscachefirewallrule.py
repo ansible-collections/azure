23d26739bcd9c76b23c207076c6004a63856e74b
Ross Bender <l3ender@users.noreply.github.com>
2021-11-10 11:02:28 -0600

    use correct poller for updated redis sdk

--- plugins/modules/azure_rm_rediscachefirewallrule.py
+++ plugins/modules/azure_rm_rediscachefirewallrule.py
@@ -84,8 +84,7 @@ import time
 
 try:
     from ansible_collections.azure.azcollection.plugins.module_utils.azure_rm_common import AzureRMModuleBase
-    from msrest.polling import LROPoller
-    from msrestazure.azure_operation import AzureOperationPoller
+    from azure.core.polling import LROPoller
     from azure.core.exceptions import ResourceNotFoundError
     from azure.mgmt.redis import RedisManagementClient
 except ImportError:
@@ -254,7 +253,7 @@ class AzureRMRedisCacheFirewallRule(AzureRMModuleBase):
                                                                     rule_name=self.name,
                                                                     start_ip=self.start_ip_address,
                                                                     end_ip=self.end_ip_address)
-            if isinstance(response, LROPoller) or isinstance(response, AzureOperationPoller):
+            if isinstance(response, LROPoller):
                 response = self.get_poller_result(response)
 
         except Exception as exc:
