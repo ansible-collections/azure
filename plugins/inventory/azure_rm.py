6982092ec1b41a86dd44b1aeb3c6a0a978fc74b9
Fred-sun <xiuxi.sun@qq.com>
2022-03-10 10:57:32 +0800

    add new change

--- plugins/inventory/azure_rm.py
+++ plugins/inventory/azure_rm.py
@@ -5,8 +5,8 @@ from __future__ import (absolute_import, division, print_function)
 __metaclass__ = type
 
 DOCUMENTATION = r'''
-    module: azure_rm
-    plugin: azure.azcollection.azure_rm
+    name: azure_rm
+    plugin_type: inventory
     short_description: Azure Resource Manager inventory plugin
     extends_documentation_fragment:
       - azure.azcollection.azure
