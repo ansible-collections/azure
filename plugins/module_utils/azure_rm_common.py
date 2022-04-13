40ee355288f280a04312a00c9f5a2dc439776dcd
Ross Bender <l3ender@users.noreply.github.com>
2022-04-13 17:33:00 -0500

    update to latest containerregistry sdk

--- plugins/module_utils/azure_rm_common.py
+++ plugins/module_utils/azure_rm_common.py
@@ -1210,6 +1210,7 @@ class AzureRMModuleBase(object):
         if not self._containerregistry_client:
             self._containerregistry_client = self.get_mgmt_svc_client(ContainerRegistryManagementClient,
                                                                       base_url=self._cloud_environment.endpoints.resource_manager,
+                                                                      is_track2=True,
                                                                       api_version='2017-10-01')
 
         return self._containerregistry_client
