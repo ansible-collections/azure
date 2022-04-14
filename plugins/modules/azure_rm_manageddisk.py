905eeb11c4555655ac8819e74b8ae7384d18b2a1
Max <max@23.tf>
2022-04-14 07:57:58 +0000

    Fix os_type comparison in azure_rm_manageddisk with existing disk (#621)
    * Fix os_type comparison in azure_rm_manageddisk with existing disk
    
    * Update str to OperatingSystemTypes conversion, leveraging the sdk capabilities
    
    * Fix bug in os_type comparison when curren os_type is None
    
    Co-authored-by: xuzhang3 <57888764+xuzhang3@users.noreply.github.com>

--- plugins/modules/azure_rm_manageddisk.py
+++ plugins/modules/azure_rm_manageddisk.py
@@ -455,11 +455,7 @@ class AzureRMManagedDisk(AzureRMModuleBase):
             creation_data['create_option'] = self.compute_models.DiskCreateOption.copy
             creation_data['source_resource_id'] = self.source_uri
         if self.os_type:
-            typecon = {
-                'linux': self.compute_models.OperatingSystemTypes.linux,
-                'windows': self.compute_models.OperatingSystemTypes.windows
-            }
-            disk_params['os_type'] = typecon[self.os_type]
+            disk_params['os_type'] = self.compute_models.OperatingSystemTypes(self.os_type.capitalize())
         else:
             disk_params['os_type'] = None
         disk_params['creation_data'] = creation_data
@@ -483,7 +479,7 @@ class AzureRMManagedDisk(AzureRMModuleBase):
             if not found_disk['disk_size_gb'] == new_disk['disk_size_gb']:
                 resp = True
         if new_disk.get('os_type'):
-            if not found_disk['os_type'] == new_disk['os_type']:
+            if found_disk['os_type'] is None or not self.compute_models.OperatingSystemTypes(found_disk['os_type'].capitalize()) == new_disk['os_type']:
                 resp = True
         if new_disk.get('sku'):
             if not found_disk['storage_account_type'] == new_disk['sku'].name:
