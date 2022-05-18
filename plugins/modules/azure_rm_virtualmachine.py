68b01c51c9ce47768e6ceb7ad1fcce0969b5a5b7
Andrea Decorte <adecorte@redhat.com>
2022-05-18 21:39:28 +0200

    Add support for ZRS disks in virtualdisk and manageddisk modules

--- plugins/modules/azure_rm_virtualmachine.py
+++ plugins/modules/azure_rm_virtualmachine.py
@@ -182,7 +182,9 @@ options:
         choices:
             - Standard_LRS
             - StandardSSD_LRS
+            - StandardSSD_ZRS
             - Premium_LRS
+            - Premium_ZRS
     os_disk_name:
         description:
             - OS disk name.
@@ -232,7 +234,9 @@ options:
                 choices:
                     - Standard_LRS
                     - StandardSSD_LRS
+                    - StandardSSD_ZRS
                     - Premium_LRS
+                    - Premium_ZRS
             storage_account_name:
                 description:
                     - Name of an existing storage account that supports creation of VHD blobs.
@@ -879,7 +883,7 @@ class AzureRMVirtualMachine(AzureRMModuleBase):
             storage_blob_name=dict(type='str', aliases=['storage_blob']),
             os_disk_caching=dict(type='str', aliases=['disk_caching'], choices=['ReadOnly', 'ReadWrite']),
             os_disk_size_gb=dict(type='int'),
-            managed_disk_type=dict(type='str', choices=['Standard_LRS', 'StandardSSD_LRS', 'Premium_LRS']),
+            managed_disk_type=dict(type='str', choices=['Standard_LRS', 'StandardSSD_LRS', 'StandardSSD_ZRS', 'Premium_LRS', 'Premium_ZRS']),
             os_disk_name=dict(type='str'),
             proximity_placement_group=dict(type='dict', options=proximity_placement_group_spec),
             os_type=dict(type='str', choices=['Linux', 'Windows'], default='Linux'),
