deb1289e73ae273b1c8e8180f122cda90ea5d1c8
Fred-sun <xiuxi.sun@qq.com>
2022-06-20 21:57:27 +0800

    small change

--- plugins/modules/azure_rm_servicebussaspolicy.py
+++ plugins/modules/azure_rm_servicebussaspolicy.py
@@ -248,9 +248,9 @@ class AzureRMServiceBusSASPolicy(AzureRMModuleBase):
         try:
             client = self._get_client()
             if self.queue or self.topic:
-                rule = client.create_or_update_authorization_rule(self.resource_group, self.namespace, self.queue or self.topic, self.name, rights)
+                rule = client.create_or_update_authorization_rule(self.resource_group, self.namespace, self.queue or self.topic, self.name, parameters={'rights': rights})
             else:
-                rule = client.create_or_update_authorization_rule(self.resource_group, self.namespace, self.name, rights)
+                rule = client.create_or_update_authorization_rule(self.resource_group, self.namespace, self.name, parameters={'rights': rights})
             return rule
         except Exception as exc:
             self.fail('Error when creating or updating SAS policy {0} - {1}'.format(self.name, exc.message or str(exc)))
