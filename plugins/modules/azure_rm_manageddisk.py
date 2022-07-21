bc861bfb4cab78128cef76f387259777e3bff716
Fred-sun <xiuxi.sun@qq.com>
2022-07-21 16:12:24 +0800

    Add missing parameters

--- plugins/modules/azure_rm_manageddisk.py
+++ plugins/modules/azure_rm_manageddisk.py
@@ -637,7 +637,7 @@ class AzureRMManagedDisk(AzureRMModuleBase):
     def is_attach_caching_option_different(self, vm_name, disk):
         resp = False
         if vm_name:
-            vm = self._get_vm(vm_name)
+            vm = self._get_vm(self.resource_group, vm_name)
             correspondence = next((d for d in vm.storage_profile.data_disks if d.name.lower() == disk.get('name').lower()), None)
             caching_options = self.compute_models.CachingTypes[self.attach_caching] if self.attach_caching and self.attach_caching != '' else None
             if correspondence and correspondence.caching != caching_options:
