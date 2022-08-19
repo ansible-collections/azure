d8e40754e30c0e746d5e6674f89c7462bc5a2b40
Fred-sun <xiuxi.sun@qq.com>
2022-08-19 15:18:45 +0800

    copyright change

--- plugins/modules/azure_rm_virtualhubconnection.py
+++ plugins/modules/azure_rm_virtualhubconnection.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 #
-# Copyright (c) 2020 XiuxiSun, (@Fred-sun)
+# Copyright (c) 2022 xuzhang3 (@xuzhang3)
+#                    XiuxiSun, (@Fred-sun)
 #
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
