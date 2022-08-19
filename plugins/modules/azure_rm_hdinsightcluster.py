b70b40a3d82b183aa5ac6388102ae9dc0fdce0a6
Fred-sun <37327967+Fred-sun@users.noreply.github.com>
2022-08-19 16:13:17 +0800

    Upgrade hdinsight dependence to 9.0.0 (#951)
    * Upgrade azure-mgmt-hdinsight to 9.0.0
    
    * add new change
    
    * add new change
    
    * small change
    
    * Add whitespace after ':'

--- plugins/modules/azure_rm_hdinsightcluster.py
+++ plugins/modules/azure_rm_hdinsightcluster.py
@@ -173,8 +173,8 @@ import time
 from ansible_collections.azure.azcollection.plugins.module_utils.azure_rm_common import AzureRMModuleBase
 
 try:
-    from msrestazure.azure_exceptions import CloudError
-    from msrest.polling import LROPoller
+    from azure.core.polling import LROPoller
+    from azure.core.exceptions import ResourceNotFoundError
     from msrestazure.azure_operation import AzureOperationPoller
     from azure.mgmt.hdinsight import HDInsightManagementClient
     from msrest.serialization import Model
@@ -281,6 +281,7 @@ class AzureRMClusters(AzureRMModuleBase):
         response = None
 
         self.mgmt_client = self.get_mgmt_svc_client(HDInsightManagementClient,
+                                                    is_track2=True,
                                                     base_url=self._cloud_environment.endpoints.resource_manager)
 
         resource_group = self.get_resource_group(self.resource_group)
@@ -356,25 +357,26 @@ class AzureRMClusters(AzureRMModuleBase):
 
         try:
             if self.to_do == Actions.Create:
-                response = self.mgmt_client.clusters.create(resource_group_name=self.resource_group,
-                                                            cluster_name=self.name,
-                                                            parameters=self.parameters)
+                response = self.mgmt_client.clusters.begin_create(resource_group_name=self.resource_group,
+                                                                  cluster_name=self.name,
+                                                                  parameters=self.parameters)
                 if isinstance(response, LROPoller) or isinstance(response, AzureOperationPoller):
                     response = self.get_poller_result(response)
             else:
                 if self.tags_changed:
                     response = self.mgmt_client.clusters.update(resource_group_name=self.resource_group,
                                                                 cluster_name=self.name,
-                                                                tags=self.parameters.get('tags'))
+                                                                parameters={'tags': self.parameters.get('tags')})
                     if isinstance(response, LROPoller) or isinstance(response, AzureOperationPoller):
                         response = self.get_poller_result(response)
                 if self.new_instance_count:
-                    response = self.mgmt_client.clusters.resize(resource_group_name=self.resource_group,
-                                                                cluster_name=self.name,
-                                                                target_instance_count=self.new_instance_count)
+                    response = self.mgmt_client.clusters.begin_resize(resource_group_name=self.resource_group,
+                                                                      cluster_name=self.name,
+                                                                      role_name='workernode',
+                                                                      parameters={'target_instance_count': self.new_instance_count})
                     if isinstance(response, LROPoller) or isinstance(response, AzureOperationPoller):
                         response = self.get_poller_result(response)
-        except CloudError as exc:
+        except Exception as exc:
             self.fail("Error creating or updating Cluster instance: {0}".format(str(exc)))
         return response.as_dict() if response else {}
 
@@ -386,9 +388,9 @@ class AzureRMClusters(AzureRMModuleBase):
         '''
         self.log("Deleting the Cluster instance {0}".format(self.name))
         try:
-            response = self.mgmt_client.clusters.delete(resource_group_name=self.resource_group,
-                                                        cluster_name=self.name)
-        except CloudError as e:
+            response = self.mgmt_client.clusters.begin_delete(resource_group_name=self.resource_group,
+                                                              cluster_name=self.name)
+        except Exception as e:
             self.fail("Error deleting the Cluster instance: {0}".format(str(e)))
 
         return True
@@ -407,7 +409,7 @@ class AzureRMClusters(AzureRMModuleBase):
             found = True
             self.log("Response : {0}".format(response))
             self.log("Cluster instance : {0} found".format(response.name))
-        except Exception as e:
+        except ResourceNotFoundError as e:
             self.log('Did not find the Cluster instance.')
         if found is True:
             return response.as_dict()
