2061bdefacb536d110f92b4268a1078ce5b4a4ad
Fred-sun <xiuxi.sun@qq.com>
2023-06-26 17:45:46 +0800

    last sanity fail update

--- plugins/modules/azure_rm_keyvaultkey_info.py
+++ plugins/modules/azure_rm_keyvaultkey_info.py
@@ -266,13 +266,12 @@ def deletedkeybundle_to_dict(bundle):
     keybundle['recovery_id'] = bundle.recovery_id
     keybundle['scheduled_purge_date'] = bundle.scheduled_purge_date
     keybundle['deleted_date'] = bundle.deleted_date
-    keybundle['key'] = dict(
-                            n=bundle.key.n if hasattr(bundle.key, 'n') else None,
+    keybundle['key'] = dict(n=bundle.key.n if hasattr(bundle.key, 'n') else None,
                             e=bundle.key.e if hasattr(bundle.key, 'e') else None,
                             crv=bundle.key.crv if hasattr(bundle.key, 'crv') else None,
                             x=bundle.key.x if hasattr(bundle.key, 'x') else None,
                             y=bundle.key.y if hasattr(bundle.key, 'y') else None)
-    keybundle['id']=bundle.id,
+    keybundle['id'] = bundle.id,
     return keybundle
 
 
