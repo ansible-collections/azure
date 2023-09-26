1b98fe3617f464152edad414fb3960bd377fef7e
neil-yechenwei <v-cheye@microsoft.com>
2023-09-26 15:20:51 +0800

    update azure_rm_aduser.py

--- plugins/modules/azure_rm_aduser.py
+++ plugins/modules/azure_rm_aduser.py
@@ -274,9 +274,9 @@ class AzureRMADUser(AzureRMModuleBase):
             setattr(self, key, kwargs[key])
 
         try:
-            client = self.get_msgraph_client(self.tenant)
+            self._client = self.get_msgraph_client(self.tenant)
 
-            ad_user = self.get_exisiting_user(client)            
+            ad_user = self.get_exisiting_user()            
 
             if self.state == 'present':
 
@@ -312,61 +312,27 @@ class AzureRMADUser(AzureRMModuleBase):
                     if should_update or self.mail_nickname and ad_user.mail_nickname != self.mail_nickname:
                         should_update = True
 
-                    if should_update:
-                        request_body = User(
-                                on_premises_immutable_id = self.immutable_id,
-                                usage_location = self.usage_location,
-                                given_name = self.given_name,
-                                surname = self.surname,
-                                user_type = self.user_type,
-                                account_enabled = self.account_enabled,
-                                display_name = self.display_name,
-                                password_profile = password,
-                                user_principal_name = self.user_principal_name,
-                                mail_nickname = self.mail_nickname
-                            )
-                        async def update_user():
-                            return await client.users.by_user_id(ad_user.object_id).patch()(body = request_body)
-                        asyncio.run(update_user())
+                    if should_update:                        
+                        asyncio.get_event_loop().run_until_complete(self.update_user(ad_user, password))
 
                         self.results['changed'] = True
 
                         # Get the updated versions of the users to return
                         # the update method, has no return value so it needs to be explicitely returned in a call
-                        ad_user = self.get_exisiting_user(client)
+                        ad_user = self.get_exisiting_user()
 
                     else:
                         self.results['changed'] = False
 
                 else:  # Create, changed
-                    password = PasswordProfile(
-                            password = self.password_profile
-                        )
-                    request_body = User(
-                        account_enabled = self.account_enabled,
-                        display_name = self.display_name,
-                        password_profile = password,
-                        user_principal_name = self.user_principal_name,
-                        mail_nickname = self.mail_nickname,
-                        on_premises_immutable_id = self.immutable_id,
-                        usage_location = self.usage_location,
-                        given_name = self.given_name,
-                        surname = self.surname,
-                        user_type = self.user_type,
-                        mail = self.mail
-                    )
-                    async def create_user():
-                        return await client.users.post(body = request_body)
-                    ad_user = asyncio.run(create_user())
+                    ad_user = asyncio.get_event_loop().run_until_complete(self.create_user())
                     self.results['changed'] = True
 
                 self.results['ad_user'] = self.to_dict(ad_user)
 
             elif self.state == 'absent':
-                if ad_user:  # Delete, changed
-                    async def delete_user():
-                        return await client.users.by_user_id(ad_user.object_id).delete()
-                    asyncio.run(delete_user())
+                if ad_user:  # Delete, changed                    
+                    asyncio.get_event_loop().run_until_complete(self.delete_user(ad_user))
                     self.results['changed'] = True
                 else:  # Do nothing unchanged
                     self.results['changed'] = False
@@ -376,41 +342,30 @@ class AzureRMADUser(AzureRMModuleBase):
 
         return self.results
 
-    def get_exisiting_user(self, client):
+    def get_exisiting_user(self):
         ad_user = None
 
         try:
-            async def get_user(object):
-                return await client.users.by_user_id(object).get()
-            
-            async def get_users_by_filter(filter):
-                return await client.users.get(request_configuration = UsersRequestBuilder.UsersRequestBuilderGetRequestConfiguration(
-                            query_parameters = UsersRequestBuilder.UsersRequestBuilderGetQueryParameters(
-                                filter = filter,
-                            ),
-                        )
-                    )
-
             if self.user_principal_name is not None:
-                ad_user = asyncio.run(get_user(self.user_principal_name))
+                ad_user = asyncio.get_event_loop().run_until_complete(self.get_user(self.user_principal_name))
             elif self.object_id is not None:
-                ad_user = asyncio.run(get_user(self.object_id))
+                ad_user = asyncio.get_event_loop().run_until_complete(self.get_user(self.object_id))
             elif self.attribute_name is not None and self.attribute_value is not None:
                 try:
-                    users = asyncio.run(get_users_by_filter("{0} eq '{1}'".format(self.attribute_name, self.attribute_value)))    
+                    users = asyncio.get_event_loop().run_until_complete(self.get_users_by_filter("{0} eq '{1}'".format(self.attribute_name, self.attribute_value)))    
                     ad_users = list(users.value)
                     ad_user = ad_users[0]
                 except Exception as e:
                     # the type doesn't get more specific. Could check the error message but no guarantees that message doesn't change in the future
                     # more stable to try again assuming the first error came from the attribute being a list
                     try:
-                        users = asyncio.run(get_users_by_filter("{0}/any(c:c eq '{1}')".format(self.attribute_name, self.attribute_value)))    
+                        users = asyncio.get_event_loop().run_until_complete(self.get_users_by_filter("{0}/any(c:c eq '{1}')".format(self.attribute_name, self.attribute_value)))    
                         ad_users = list(users.value)
                         ad_user = ad_users[0]
                     except Exception as sub_e:
                         raise
             elif self.odata_filter is not None:  # run a filter based on user input to return based on any given attribute/query
-                users = asyncio.run(get_users_by_filter(self.odata_filter))    
+                users = asyncio.get_event_loop().run_until_complete(self.get_users_by_filter(self.odata_filter))    
                 ad_users = list(users.value)
                 ad_user = ad_users[0]
         except Exception as e:
@@ -433,6 +388,53 @@ class AzureRMADUser(AzureRMModuleBase):
             user_type=object.user_type
         )
 
+    async def update_user(self, ad_user, password):
+        request_body = User(
+                on_premises_immutable_id = self.immutable_id,
+                usage_location = self.usage_location,
+                given_name = self.given_name,
+                surname = self.surname,
+                user_type = self.user_type,
+                account_enabled = self.account_enabled,
+                display_name = self.display_name,
+                password_profile = password,
+                user_principal_name = self.user_principal_name,
+                mail_nickname = self.mail_nickname
+            )
+        return await self._client.users.by_user_id(ad_user.object_id).patch()(body = request_body)
+
+    async def create_user(self):
+        password = PasswordProfile(
+                password = self.password_profile
+            )
+        request_body = User(
+            account_enabled = self.account_enabled,
+            display_name = self.display_name,
+            password_profile = password,
+            user_principal_name = self.user_principal_name,
+            mail_nickname = self.mail_nickname,
+            on_premises_immutable_id = self.immutable_id,
+            usage_location = self.usage_location,
+            given_name = self.given_name,
+            surname = self.surname,
+            user_type = self.user_type,
+            mail = self.mail
+        )
+        return await self._client.users.post(body = request_body)
+
+    async def delete_user(self, ad_user):
+        return await self._client.users.by_user_id(ad_user.object_id).delete()
+
+    async def get_user(self, object):
+        return await self._client.users.by_user_id(object).get()
+
+    async def get_users_by_filter(self, filter):
+        return await self._client.users.get(request_configuration = UsersRequestBuilder.UsersRequestBuilderGetRequestConfiguration(
+                    query_parameters = UsersRequestBuilder.UsersRequestBuilderGetQueryParameters(
+                        filter = filter,
+                    )
+                )
+            )
 
 def main():
     AzureRMADUser()
