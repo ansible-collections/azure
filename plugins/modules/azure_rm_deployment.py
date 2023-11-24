0d1a454cbcfa8d2a435bd71a87135753afe1a251
Fred-sun <37327967+Fred-sun@users.noreply.github.com>
2023-11-24 16:47:57 +0800

    Fix azure_rm_deployment deletes the whole resource group in state absent (#1231)
    * fix azure_rm_deployment.py bug
    
    * small change

--- plugins/modules/azure_rm_deployment.py
+++ plugins/modules/azure_rm_deployment.py
@@ -84,7 +84,7 @@ options:
         description:
             - If I(state=present), template will be created.
             - If I(state=present) and deployment exists, it will be updated.
-            - If I(state=absent), the resource group will be removed.
+            - If I(state=absent), the deployment resource will be deleted.
         default: present
         type: str
         choices:
@@ -498,7 +498,7 @@ class AzureRMDeploymentManager(AzureRMModuleBase):
         else:
             try:
                 if self.get_resource_group(self.resource_group):
-                    self.destroy_resource_group()
+                    self.destroy_deployment_resource()
                     self.results['changed'] = True
                     self.results['msg'] = "deployment deleted"
             except Exception:
@@ -572,12 +572,12 @@ class AzureRMDeploymentManager(AzureRMModuleBase):
 
         return deployment_result
 
-    def destroy_resource_group(self):
+    def destroy_deployment_resource(self):
         """
         Destroy the targeted resource group
         """
         try:
-            result = self.rm_client.resource_groups.begin_delete(self.resource_group)
+            result = self.rm_client.deployments.begin_delete(self.resource_group, self.name)
             result.wait()  # Blocking wait till the delete is finished
         except Exception as e:
             if e.status_code == 404 or e.status_code == 204:
