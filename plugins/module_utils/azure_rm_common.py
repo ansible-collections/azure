8b3b80fae1c1c51f268f22f18d761f72b17e9c5a
Jeremy Cline <jeremycline@microsoft.com>
2024-02-27 09:47:24 -0500

    Support for azure-mgmt-recoveryservicesbackup v4.0+
    In newer versions of the library, the models module got split between
    two sub-packages, `passivestamp` and `activestamp`. All models used are
    part of the `activestamp` module, so conditionally import from there if
    the newer version is installed.

--- plugins/module_utils/azure_rm_common.py
+++ plugins/module_utils/azure_rm_common.py
@@ -271,7 +271,11 @@ try:
     from azure.mgmt.iothub import models as IoTHubModels
     from azure.mgmt.resource.locks import ManagementLockClient
     from azure.mgmt.recoveryservicesbackup import RecoveryServicesBackupClient
-    import azure.mgmt.recoveryservicesbackup.models as RecoveryServicesBackupModels
+    try:
+        #  Older versions of the library exposed the modules at the root of the package
+        import azure.mgmt.recoveryservicesbackup.models as RecoveryServicesBackupModels
+    except ImportError:
+        import azure.mgmt.recoveryservicesbackup.activestamp.models as RecoveryServicesBackupModels
     from azure.mgmt.search import SearchManagementClient
     from azure.mgmt.datalake.store import DataLakeStoreAccountManagementClient
     import azure.mgmt.datalake.store.models as DataLakeStoreAccountModel
