32d4de074d6e4161451f097bcc61eca9e7a7fc9a
Fred-sun <xiuxi.sun@qq.com>
2025-02-26 14:59:12 +0800

    small change

--- plugins/modules/azure_rm_diskaccess_info.py
+++ plugins/modules/azure_rm_diskaccess_info.py
@@ -11,7 +11,7 @@ __metaclass__ = type
 DOCUMENTATION = '''
 ---
 module: azure_rm_diskaccess_info
-version_added: '3.2.0'
+version_added: '3.3.0'
 short_description: Show the details for the disk access
 description:
     - Get or list the details for the disk access.
