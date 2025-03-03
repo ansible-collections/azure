b411c6bae202a7344b384a666de2ca3fbe5e052f
Bill Peck <bpeck@redhat.com>
2025-03-03 04:26:19 -0500

    Add cache support to inventory plugin (#1828)
    * Add cache support to inventory plugin
    
    The default cache_plugin is memory which won't really help. Change to
    jsonfile to cache on disk.
    
    cache: true
    cache_plugin: ansible.builtin.jsonfile
    cache_timeout: 7200
    cache_connection: /tmp/azure_inventory
    
    * Update cache logic
    
    Fixed a mistake with the cache logic when flushing the cache didn't
    update the cache on disk.  This would mean if you ran again after a
    flush you could get the old stale cache.
    
    * Add inventory cache integration tests
    
    Also fixed trying to load the cache when it wasn't enabled.

--- plugins/inventory/azure_rm.py
+++ plugins/inventory/azure_rm.py
@@ -20,6 +20,7 @@ DOCUMENTATION = r'''
       - azure.azcollection.azure
       - azure.azcollection.azure_rm
       - constructed
+      - inventory_cache
     description:
         - Query VM details from Azure Resource Manager
         - Requires a YAML configuration file whose name ends with 'azure_rm.(yml|yaml)'
@@ -154,7 +155,7 @@ except ImportError:
     from Queue import Queue, Empty
 
 from collections import namedtuple
-from ansible.plugins.inventory import BaseInventoryPlugin, Constructable
+from ansible.plugins.inventory import BaseInventoryPlugin, Constructable, Cacheable
 from ansible.module_utils.six import iteritems
 from ansible_collections.azure.azcollection.plugins.module_utils.azure_rm_common import AzureRMAuth
 from ansible.errors import AnsibleParserError, AnsibleError
@@ -202,8 +203,7 @@ class AzureRMRestConfiguration(Configuration):
 UrlAction = namedtuple('UrlAction', ['url', 'api_version', 'handler', 'handler_args'])
 
 
-# FUTURE: add Cacheable support once we have a sane serialization format
-class InventoryModule(BaseInventoryPlugin, Constructable):
+class InventoryModule(BaseInventoryPlugin, Constructable, Cacheable):
 
     NAME = 'azure.azcollection.azure_rm'
 
@@ -257,11 +257,46 @@ class InventoryModule(BaseInventoryPlugin, Constructable):
 
         self._include_filters = self.get_option('include_host_filters')
 
-        try:
-            self._credential_setup()
-            self._get_hosts()
-        except Exception:
-            raise
+        # Load results from Cache if requested
+        cache_key = self.get_cache_key(path)
+
+        # cache may be True or False at this point to indicate if the inventory is being refreshed
+        # get the user's cache option too to see if we should save the cache if it is changing
+        user_cache_setting = self.get_option('cache')
+
+        # read if the user has caching enabled and the cache isn't being refreshed
+        attempt_to_read_cache = user_cache_setting and cache
+        # update if the user has caching enabled and the cache is being refreshed;
+        # update this value to True if the cache has expired below
+        cache_needs_update = user_cache_setting and not cache
+
+        # attempt to read the cache if inventory isn't being refreshed and the user has caching enabled
+        if attempt_to_read_cache:
+            try:
+                results = self._cache[cache_key]
+            except KeyError:
+                # This occurs if the cache_key is not in the cache or if the cache_key
+                # expired, so the cache needs to be updated
+                cache_needs_update = True
+        if not attempt_to_read_cache or cache_needs_update:
+            # parse the provided inventory source
+            try:
+                self._credential_setup()
+                self._get_hosts()
+                results = self._serialize(self._hosts)
+            except Exception:
+                raise
+        if cache_needs_update:
+            self._cache[cache_key] = results
+
+        self._populate(results)
+
+    def _serialize(self, hosts):
+        results = []
+        for h in hosts:
+            results.append(dict(default_inventory_hostname=h.default_inventory_hostname,
+                                hostvars=h.hostvars))
+        return results
 
     def _credential_setup(self):
         auth_source = environ.get('ANSIBLE_AZURE_AUTH_SOURCE', None) or self.get_option('auth_source')
@@ -367,6 +402,7 @@ class InventoryModule(BaseInventoryPlugin, Constructable):
         else:
             self._process_queue_serial()
 
+    def _populate(self, results):
         constructable_config_strict = boolean(self.get_option('fail_on_template_errors'))
         if self.get_option('hostvar_expressions') is not None:
             constructable_config_compose = self.get_option('hostvar_expressions')
@@ -377,25 +413,26 @@ class InventoryModule(BaseInventoryPlugin, Constructable):
 
         constructable_hostnames = self.get_option('hostnames')
 
-        for h in self._hosts:
+        for h in results:
+            hostvars = h.get("hostvars")
             # FUTURE: track hostnames to warn if a hostname is repeated (can happen for legacy and for composed inventory_hostname)
             inventory_hostname = self._get_hostname(h, hostnames=constructable_hostnames, strict=constructable_config_strict)
-            if self._filter_exclude_host(inventory_hostname, h.hostvars):
+            if self._filter_exclude_host(inventory_hostname, hostvars):
                 continue
-            if not self._filter_include_host(inventory_hostname, h.hostvars):
+            if not self._filter_include_host(inventory_hostname, hostvars):
                 continue
             self.inventory.add_host(inventory_hostname)
             # FUTURE: configurable default IP list? can already do this via hostvar_expressions
             self.inventory.set_variable(inventory_hostname, "ansible_host",
-                                        next(chain(h.hostvars['public_ipv4_address'], h.hostvars['private_ipv4_addresses']), None))
-            for k, v in iteritems(h.hostvars):
+                                        next(chain(hostvars['public_ipv4_address'], hostvars['private_ipv4_addresses']), None))
+            for k, v in iteritems(hostvars):
                 # FUTURE: configurable hostvar prefix? Makes docs harder...
                 self.inventory.set_variable(inventory_hostname, k, v)
 
             # constructable delegation
-            self._set_composite_vars(constructable_config_compose, h.hostvars, inventory_hostname, strict=constructable_config_strict)
-            self._add_host_to_composed_groups(constructable_config_groups, h.hostvars, inventory_hostname, strict=constructable_config_strict)
-            self._add_host_to_keyed_groups(constructable_config_keyed_groups, h.hostvars, inventory_hostname, strict=constructable_config_strict)
+            self._set_composite_vars(constructable_config_compose, hostvars, inventory_hostname, strict=constructable_config_strict)
+            self._add_host_to_composed_groups(constructable_config_groups, hostvars, inventory_hostname, strict=constructable_config_strict)
+            self._add_host_to_keyed_groups(constructable_config_keyed_groups, hostvars, inventory_hostname, strict=constructable_config_strict)
 
     # FUTURE: fix underlying inventory stuff to allow us to quickly access known groupvars from reconciled host
     def _filter_host(self, filter, inventory_hostname, hostvars):
@@ -426,9 +463,9 @@ class InventoryModule(BaseInventoryPlugin, Constructable):
 
         for preference in hostnames:
             if preference == 'default':
-                return host.default_inventory_hostname
+                return host.get("default_inventory_hostname")
             try:
-                hostname = self._compose(preference, host.hostvars)
+                hostname = self._compose(preference, host.get("hostvars"))
             except Exception as e:  # pylint: disable=broad-except
                 if strict:
                     raise AnsibleError("Could not compose %s as hostnames - %s" % (preference, to_native(e)))
--- tests/integration/targets/inventory_azure/runme.sh
+++ tests/integration/targets/inventory_azure/runme.sh
@@ -24,6 +24,15 @@ ansible-playbook playbooks/empty_inventory_config.yml "$@"
 ansible-playbook playbooks/create_inventory_config.yml "$@"  --extra-vars "template=filter.yml"
 ansible-playbook playbooks/test_inventory_filter.yml "$@"
 
+# using cache
+ansible-playbook playbooks/empty_inventory_config.yml "$@"
+ansible-playbook playbooks/create_inventory_config.yml "$@"  --extra-vars "template=cache.yml"
+ansible-playbook playbooks/test_inventory_cache.yml "$@"
+ansible-playbook playbooks/test_inventory_flush_part_1.yml "$@"
+ansible-playbook playbooks/test_inventory_flush_part_2.yml "$@"
+ansible-playbook --flush-cache playbooks/test_inventory_flush_part_3.yml "$@"
+ansible-playbook playbooks/test_inventory_flush_part_3.yml "$@"
+
 
 # teardown
 ansible-playbook playbooks/teardown.yml "$@"
