e2f6821a44f3ce6829e6a31a54edfe928000982e
Ubuntu <azureuser@testVM.r0eqgidmcfzubmqlq0l1anvtac.bx.internal.cloudapp.net>
2025-03-05 02:42:19 +0000

    Add retrun info

--- plugins/modules/azure_rm_resourcehealthstates_info.py
+++ plugins/modules/azure_rm_resourcehealthstates_info.py
@@ -60,15 +60,104 @@ EXAMPLES = '''
 
 - name: Gets current availability status for a single resource.
   azure_rm_resourcehealthstates_info:
-    resource_uri: "{{ resource_uri }}"
+    resource_uri: "/subscriptions/xxxx-xxx/resourcegroups/v-xisurg/providers/microsoft.compute/virtualmachines/testvm"
 
 - name: Lists all historical availability transitions and impacting events for a single resource.
   azure_rm_resourcehealthstates_info:
-    resource_uri: "{{ resource_uri }}"
+    resource_uri: "/subscriptions/xxxx-xxx/resourcegroups/v-xisurg/providers/microsoft.compute/virtualmachines/testvm"
     list_all: true
 '''
 
 RETURN = '''
+health_states:
+    description:
+        - The facts of availabilityStatus of a resource.
+    type: complex
+    contains:
+        id:
+            description:
+                - Azure Resource Manager Identity for the availabilityStatuses resource.
+            type: str
+            returned: always
+            sample: "/subscriptions/xxxx-xxx/resourcegroups/defaultresourcegroup-wus2/providers/microsoft.operationalinsights/workspaces/
+                    defaultworkspace-e393adb3-b5be-4789-bdc9-848367f0d152-wus2/providers/Microsoft.ResourceHealth/availabilityStatuses/current"
+        location:
+            description:
+                - Azure Resource Manager geo location of the resource.
+            type: str
+            returned: always
+            sample: westus2
+        type:
+            description:
+                - Microsoft.ResourceHealth/AvailabilityStatuses.
+            type: str
+            returned: always
+            sample: "Microsoft.ResourceHealth/AvailabilityStatuses"
+        properties:
+            description:
+                - Properties of availability state.
+            type: complex
+            contains:
+                availability_state:
+                    description:
+                        - Availability status of the resource.
+                        - When it is null, this availabilityStatus object represents an availability impacting event.
+                    type: str
+                    returned: always
+                    sample: "Unknown"
+                category:
+                    description:
+                        - When a context field is set to Platform, this field will reflect if the event was planned or unplanned.
+                        - If the context field does not have a value of Platform, then this
+     field will be ignored.
+                    type: str
+                    returned: always
+                    sample: "Not Applicable"
+                context:
+                    description:
+                        - When an event is created, it can either be triggered by a customer or the platform of the resource and this field will illustrate that.
+                        - This field is connected to the
+     category field in this object.
+                    type: str
+                    returned: always
+                    sample: "Not Applicable"
+                occured_time:
+                    description:
+                        - Timestamp for when last change in health status occurred.
+                    type: str
+                    returned: always
+                    sample: "2025-03-05T01:50:29.67632Z"
+                reason_chronicity:
+                    description:
+                        - Chronicity of the availability transition.
+                    type: str
+                    returned: always
+                    sample: "Transient"
+                reason_type:
+                    description:
+                        - When the resource's availabilityState is Unavailable, it describes where the health impacting event was originated.
+                    type: str
+                    returned: always
+                    sample: ""
+                reported_time:
+                    description:
+                        - Timestamp for when the health was last checked.
+                    type: str
+                    returned: always
+                    sample: "2025-03-05T01:50:29.67632Z"
+                summary:
+                    description:
+                        - Summary description of the availability status.
+                    type: str
+                    returned: always
+                    sample: "We're currently unable to determine the health of this workspace.
+                            Either no queries were run on this workspace, or no data was ingested to this workspace recently."
+                title:
+                    description:
+                        - Title description of the availability status.
+                    type: str
+                    returned: always
+                    sample: Unkown
 '''
 
 from ansible_collections.azure.azcollection.plugins.module_utils.azure_rm_common import AzureRMModuleBase
@@ -92,7 +181,7 @@ class AzureRMResourceHealthStatesInfo(AzureRMModuleBase):
         self.resource_group = None
         self.resource_uri = None
         self.list_all = None
-        required_if = ['list_all', True, ['resource_uri']]
+        required_if = [('list_all', True, ['resource_uri'])]
 
         super(AzureRMResourceHealthStatesInfo, self).__init__(
             derived_arg_spec=self.module_args,
@@ -108,7 +197,7 @@ class AzureRMResourceHealthStatesInfo(AzureRMModuleBase):
 
         if self.resource_uri is not None:
             if self.list_all:
-                response = self.list_all(self.resource_uri)
+                response = self.list_by_resource_uri(self.resource_uri)
             else:
                 response = self.get_by_resource(self.resource_uri)
         elif self.resource_group is not None:
@@ -129,8 +218,9 @@ class AzureRMResourceHealthStatesInfo(AzureRMModuleBase):
         except Exception as exc:
             self.fail('Error when gets current availability status for a single resource. Error msg: {0}'.format(exc.message or str(exc)))
 
+        return response
 
-    def list_all(self, resource_uri):
+    def list_by_resource_uri(self, resource_uri):
         '''
         Lists all historical availability transitions and impacting events for a single resource
         :return: An iterator like instance of either AvailabilityStatus or the result of cls(response)
@@ -140,6 +230,7 @@ class AzureRMResourceHealthStatesInfo(AzureRMModuleBase):
         except Exception as exc:
             self.fail('Error when lists all historical availability transitions and impacting events for a single resource. Error msg: {0}'.format(exc.message or str(exc)))
 
+        return response
 
     def list_by_resource_group(self, resource_group):
         '''
@@ -147,9 +238,11 @@ class AzureRMResourceHealthStatesInfo(AzureRMModuleBase):
         :return: An iterator like instance of either AvailabilityStatus or the result of cls(response)
         '''
         try:
-            response = self.resourcehealth_client.availability_statuses.list_by_resource_group_name()
+            response = self.resourcehealth_client.availability_statuses.list_by_resource_group(resource_group)
         except Exception as exc:
             self.fail('Error when lists the current availability status for all the resources in the resource group. Error msg: {0}'.format(exc.message or str(exc)))
+
+        return response
 
 
     def list_by_subscription_id(self):
@@ -162,6 +255,7 @@ class AzureRMResourceHealthStatesInfo(AzureRMModuleBase):
         except Exception as exc:
             self.fail('Error when lists the current availability status for all the resources in the subscription. Error msg: {0}'.format(exc.message or str(exc)))
 
+        return response
 
 def main():
     """Main module execution code path"""
