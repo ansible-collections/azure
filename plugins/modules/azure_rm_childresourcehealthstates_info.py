04e6b80db0462d082181ea1de5c1e1f65cde9817
Fred-sun <xiuxi.sun@qq.com>
2025-03-05 11:23:50 +0800

    Add child resource health modules

--- plugins/modules/azure_rm_childresourcehealthstates_info.py
+++ plugins/modules/azure_rm_childresourcehealthstates_info.py
@@ -10,31 +10,26 @@ __metaclass__ = type
 
 DOCUMENTATION = '''
 ---
-module: azure_rm_resourcehealthstates_info
+module: azure_rm_childresourcehealthstates_info
 
 version_added: "3.3.0"
 
-short_description: Get or list the current availability status for the resource
+short_description: Get or list the current availability status for the child resource
 
 description:
-    - Get or list the current availability status for the resource.
+    - Get or list the current availability status for the child resource.
 
 options:
-    resource_group:
-        description:
-            - Name of resource group.
-        type: str
     resource_uri:
         description:
             - The fully qualified ID of the resource, including the resource name and resource type.
-            - Currently the API support not nested and one nesting level resource types,
-              such as '/subscriptions/{subscriptionId}/resourceGroups/{resource-group-name}/providers/{resource-provider-name}/
-              {resource-type}/{resource-name}' and '/subscriptions/{subscriptionId}/resourceGroups/{resourceGroupName}/
-              providers/{resource-provider-name}/{parentResourceType}/{parentResourceName}/{resourceType}/{resourceName}'.
+            - Currently the API only support one nesting level resource types, such as /subscriptions/{subscriptionId}/resourceGroups/{resourceGroupName}/
+              providers/{resource-provider-name}/{parentResourceType}/{parentResourceName}/{resourceType}/{resourceName}.
         type: str
+        required: True
     list_all:
         description:
-            - Whether lists all historical availability transitions and impacting events for a single resource.
+            - Whether lists the historical availability statuses for a single child resource.
         type: bool
         default: False
 
@@ -47,19 +42,12 @@ author:
 '''
 
 EXAMPLES = '''
-- name: Lists the current availability status for all the resources in the subscription.
-  azure_rm_resourcehealthstates_info:
-
-- name: Lists the current availability status for all the resources in the resource group.
-  azure_rm_resourcehealthstates_info:
-    resource_group: "{{ resource_group }}"
-
-- name: Gets current availability status for a single resource.
-  azure_rm_resourcehealthstates_info:
+- name: Gets current availability status for a single child resource.
+  azure_rm_childresourcehealthstates_info:
     resource_uri: "/subscriptions/xxxx-xxx/resourcegroups/v-xisurg/providers/microsoft.compute/virtualmachines/testvm"
 
-- name: Lists all historical availability transitions and impacting events for a single resource.
-  azure_rm_resourcehealthstates_info:
+- name: Lists all historical availability transitions and impacting events for a single child resource.
+  azure_rm_childresourcehealthstates_info:
     resource_uri: "/subscriptions/xxxx-xxx/resourcegroups/v-xisurg/providers/microsoft.compute/virtualmachines/testvm"
     list_all: true
 '''
@@ -158,13 +146,12 @@ health_states:
 from ansible_collections.azure.azcollection.plugins.module_utils.azure_rm_common import AzureRMModuleBase
 
 
-class AzureRMResourceHealthStatesInfo(AzureRMModuleBase):
+class AzureRMChildResourceHealthStatesInfo(AzureRMModuleBase):
 
     def __init__(self):
 
         self.module_args = dict(
-            resource_group=dict(type='str'),
-            resource_uri=dict(type='str'),
+            resource_uri=dict(type='str', required=True),
             list_all=dict(type='bool', default=False)
         )
 
@@ -173,16 +160,13 @@ class AzureRMResourceHealthStatesInfo(AzureRMModuleBase):
             health_states=[]
         )
 
-        self.resource_group = None
         self.resource_uri = None
         self.list_all = None
-        required_if = [('list_all', True, ['resource_uri'])]
 
-        super(AzureRMResourceHealthStatesInfo, self).__init__(
+        super(AzureRMChildResourceHealthStatesInfo, self).__init__(
             derived_arg_spec=self.module_args,
             supports_check_mode=True,
             supports_tags=False,
-            required_if=required_if,
             facts_module=True
         )
 
@@ -190,72 +174,44 @@ class AzureRMResourceHealthStatesInfo(AzureRMModuleBase):
         for key in self.module_args:
             setattr(self, key, kwargs[key])
 
-        if self.resource_uri is not None:
-            if self.list_all:
-                response = self.list_by_resource_uri(self.resource_uri)
-            else:
-                response = self.get_by_resource(self.resource_uri)
-        elif self.resource_group is not None:
-            response = self.list_by_resource_group(self.resource_group)
+        if self.list_all:
+            response = self.list_by_resource_uri()
         else:
-            response = self.list_by_subscription_id()
+            response = self.get_by_resource()
 
         self.results['health_states'] = [item.as_dict() for item in response]
         return self.results
 
-    def get_by_resource(self, resource_uri):
+    def get_by_resource(self):
         '''
-        Gets current availability status for a single resource.
+        Gets current availability status for a single child resource.
         :return: AvailabilityStatus or the result of cls(response)
         '''
         try:
-            response = [self.resourcehealth_client.availability_statuses.get_by_resource(resource_uri)]
+            response = [self.resourcehealth_client.child_availability_statuses.get_by_resource(resource_uri)]
         except Exception as exc:
-            self.fail('Error when gets current availability status for a single resource. Error msg: {0}'.format(exc.message or str(exc)))
+            self.fail('Error when gets current availability status for a single child resource. Error msg: {0}'.format(exc.message or str(exc)))
 
         return response
 
-    def list_by_resource_uri(self, resource_uri):
+    def list_by_resource_uri(self):
         '''
-        Lists all historical availability transitions and impacting events for a single resource
+        Lists the historical availability statuses for a single child resource.
+        Use the nextLink property in the response to get the next page of availability status.
         :return: An iterator like instance of either AvailabilityStatus or the result of cls(response)
         '''
         try:
-            response = self.resourcehealth_client.availability_statuses.list(resource_uri)
+            response = self.resourcehealth_client.child_availability_statuses.list(resource_uri)
         except Exception as exc:
-            self.fail('Error when lists all historical availability transitions and impacting events for a single resource. Error msg: {0}'.format(exc.message or str(exc)))
+            self.fail('Error when lists all historical availability transitions. Error msg: {0}'.format(exc.message or str(exc)))
 
         return response
 
-    def list_by_resource_group(self, resource_group):
-        '''
-        Lists the current availability status for all the resources in the resource group.
-        :return: An iterator like instance of either AvailabilityStatus or the result of cls(response)
-        '''
-        try:
-            response = self.resourcehealth_client.availability_statuses.list_by_resource_group(resource_group)
-        except Exception as exc:
-            self.fail('Error when lists the current availability status for all the resources in the resource group. Error msg: {0}'.format(exc.message or str(exc)))
-
-        return response
-
-
-    def list_by_subscription_id(self):
-        '''
-        Lists the current availability status for all the resources in the subscription.
-        :return: An iterator like instance of either AvailabilityStatus or the result of cls(response)
-        '''
-        try:
-            response = self.resourcehealth_client.availability_statuses.list_by_subscription_id()
-        except Exception as exc:
-            self.fail('Error when lists the current availability status for all the resources in the subscription. Error msg: {0}'.format(exc.message or str(exc)))
-
-        return response
 
 def main():
     """Main module execution code path"""
 
-    AzureRMResourceHealthStatesInfo()
+    AzureRMChildResourceHealthStatesInfo()
 
 
 if __name__ == '__main__':
