6db5113bff7cdceb3aa9b97686a169f2d25460a7
Fred-sun <xiuxi.sun@qq.com>
2025-04-17 10:47:42 +0800

    Restore mysql flexibe relate changes

--- plugins/module_utils/azure_rm_common.py
+++ plugins/module_utils/azure_rm_common.py
@@ -263,7 +263,7 @@ try:
     from azure.mgmt.rdbms.postgresql import PostgreSQLManagementClient
     from azure.mgmt.postgresqlflexibleservers import PostgreSQLManagementClient as PostgreSQLFlexibleManagementClient
     from azure.mgmt.rdbms.mysql import MySQLManagementClient
-    from azure.mgmt.mysqlflexibleservers import MySQLManagementClient as MySQLFlexibleManagementClient
+    from azure.mgmt.rdbms.mysql_flexibleservers import MySQLManagementClient as MySQLFlexibleManagementClient
     from azure.mgmt.rdbms.mariadb import MariaDBManagementClient
     from azure.mgmt.containerregistry import ContainerRegistryManagementClient
     from azure.mgmt.containerinstance import ContainerInstanceManagementClient
@@ -1254,7 +1254,7 @@ class AzureRMModuleBase(object):
         if not self._postgresql_flexible_client:
             self._postgresql_flexible_client = self.get_mgmt_svc_client(PostgreSQLFlexibleManagementClient,
                                                                         base_url=self._cloud_environment.endpoints.resource_manager,
-                                                                        api_version='2024-11-01-preview')
+                                                                        api_version='2024-08-01')
         return self._postgresql_flexible_client
 
     @property
