23fef0a3f56eab4833029869be0167548127c6c0
Fred-sun <xiuxi.sun@qq.com>
2025-04-22 09:33:21 +0800

    Support virtual networks not under the same subscription

--- plugins/modules/azure_rm_galleryimageversion.py
+++ plugins/modules/azure_rm_galleryimageversion.py
@@ -53,6 +53,7 @@ options:
                     - Reference to managed image or gallery image version
                     - Could be resource ID to managed image, or dictionary containing I(resource_group) and I(name)
                     - Could be resource ID to image version, or dictionary containing I(resource_group),I(gallery_name), I(gallery_image_name) and I(version)
+                    - The resource ID I(virtual_machine_id) of the source virtual machine. Only required when capturing a virtual machine to source this Gallery Image Version.
                     - Mutual exclusive with os_disk and data_disks
                 type: raw
             os_disk:
