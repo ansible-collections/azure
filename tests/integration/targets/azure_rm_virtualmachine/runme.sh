99c6dedf057ad1be0ea42266136d047b33a2b6ac
Fred-sun <xiuxi.sun@qq.com>
2025-07-17 17:00:44 +0800

    Restore change

--- tests/integration/targets/azure_rm_virtualmachine/runme.sh
+++ tests/integration/targets/azure_rm_virtualmachine/runme.sh
@@ -2,10 +2,4 @@
 
 set -eux
 
-# Create a resource group for VM test
-ansible-playbook -i inventory.yml create_rg.yml "$@"
-
 ansible-playbook -i inventory.yml main.yml "$@"
-
-# Force delete the resource group
-ansible-playbook -i inventory.yml delete_rg.yml "$@"
