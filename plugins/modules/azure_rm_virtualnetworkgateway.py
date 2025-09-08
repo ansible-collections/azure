a557016431c10d29ad8005ad91f29e980ad2705c
R4153 <118834958+R4153@users.noreply.github.com>
2025-09-09 01:49:37 +0200

    Add SKU type for azure_rm_virtualnetworkgateway (#2051)
    * add new sku
    
    * Changed back default value
    
    Co-authored-by: Fred-sun <37327967+Fred-sun@users.noreply.github.com>
    
    * added express gateway sku and switched to multiple lines
    
    * fixed documentation and added skus
    
    ---------
    
    Co-authored-by: Fred-sun <37327967+Fred-sun@users.noreply.github.com>

--- plugins/modules/azure_rm_virtualnetworkgateway.py
+++ plugins/modules/azure_rm_virtualnetworkgateway.py
@@ -113,6 +113,15 @@ options:
             - Standard
             - Basic
             - HighPerformance
+            - VpnGw1AZ
+            - VpnGw2AZ
+            - VpnGw3AZ
+            - VpnGw4AZ
+            - VpnGw5AZ
+            - ErGw1AZ
+            - ErGw2AZ
+            - ErGw3AZ
+            - ErGwScale
     vpn_gateway_generation:
         description:
             - The generation for this VirtualNetworkGateway. Must be C(None) if C(gateway_type) is not VPN.
@@ -273,7 +282,11 @@ class AzureRMVirtualNetworkGateway(AzureRMModuleBase):
             vpn_type=dict(type='str', default='route_based', choices=['route_based', 'policy_based']),
             vpn_gateway_generation=dict(type='str', default='Generation1', choices=['None', 'Generation1', 'Generation2']),
             enable_bgp=dict(type='bool', default=False),
-            sku=dict(type='str', default='VpnGw1', choices=['VpnGw1', 'VpnGw2', 'VpnGw3', 'Standard', 'Basic', 'HighPerformance']),
+            sku=dict(type='str', default='VpnGw1', choices=[
+                'VpnGw1', 'VpnGw2', 'VpnGw3', 'Standard', 'Basic', 'HighPerformance',
+                'VpnGw1AZ', 'VpnGw2AZ', 'VpnGw3AZ', 'VpnGw4AZ', 'VpnGw5AZ',
+                'ErGw1AZ', 'ErGw2AZ', 'ErGw3AZ', 'ErGwScale'
+            ]),
             bgp_settings=dict(type='dict', options=bgp_spec),
             virtual_network=dict(type='raw', aliases=['virtual_network_name'])
         )
