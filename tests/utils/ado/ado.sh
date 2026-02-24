a48ab0ffa3fc049809034342552d65e535a0ba4b
zunyangc <v-zuchin@microsoft.com>
2026-02-24 10:34:01 +0800

    Remove in ado.sh

--- tests/utils/ado/ado.sh
+++ tests/utils/ado/ado.sh
@@ -106,7 +106,6 @@ AZURE_TENANT:${AZURE_TENANT}
 RESOURCE_GROUP:${RESOURCE_GROUP}
 RESOURCE_GROUP_SECONDARY:${RESOURCE_GROUP_SECONDARY}
 AZURE_PRINCIPAL_ID:${AZURE_PRINCIPAL_ID}
-AZURE_MANAGED_BY_TENANT_ID:${AZURE_MANAGED_BY_TENANT_ID}
 EOF
     ansible-test integration --color -v --retry-on-error "shippable/azure/group${GROUP_NO}/" --allow-destructive || { rm "$config_file"; die "failed to run integration test"; }
     rm "$config_file"
