cdb97ee044ece0cb62624f2b64de45d3cb8f0017
zunyangc <v-zuchin@microsoft.com>
2026-03-30 13:06:38 +0800

    Remove docs

--- plugins/modules/azure_rm_virtualnetworkgateway.py
+++ plugins/modules/azure_rm_virtualnetworkgateway.py
@@ -104,9 +104,6 @@ options:
     sku:
         description:
             - The reference of the VirtualNetworkGatewaySku resource which represents the SKU selected for Virtual network gateway.
-            - Default is now C(VpnGw1AZ) instead of C(VpnGw1).
-            - VpnGw1-5 (Non-AZ SKUs) will be retired on Sep 30, 2026. Gateways will be automatically migrated to AZ SKUs.
-            - More information on the retirement https://azure.microsoft.com/updates/v2/vpngw1-5-non-az-skus-will-be-retired-on-30-september-2026.
         default: VpnGw1AZ
         type: str
         choices:
