82da3e56546cb0d96f75d3a1dbdec193bfb688d5
zunyangc <v-zuchin@microsoft.com>
2026-07-27 09:30:54 +0800

    Fix Ruff CPY001 failure on EDA event source plugins

--- extensions/eda/plugins/event_source/azure_event_hub.py
+++ extensions/eda/plugins/event_source/azure_event_hub.py
@@ -1,3 +1,6 @@
+# Copyright (c) 2025 Red Hat, Inc.
+#
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 """Ansible-rulebook event source plugin for Azure Event Hub.
 
 This module provides asynchronous consumers for Azure Event Hub to feed
