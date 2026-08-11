ef287afc56982cc95b833b007325bc35cdad7212
Bill Peck <bpeck@redhat.com>
2026-08-11 03:51:12 -0400

    Fix cognitivesearch info module to be consistent (#2332)
    This fixes the azure_rm_cognitivesearch_info module to always return
    a list for results.

--- plugins/modules/azure_rm_cognitivesearch_info.py
+++ plugins/modules/azure_rm_cognitivesearch_info.py
@@ -253,7 +253,7 @@ class AzureRMSearchInfo(AzureRMModuleBase):
     def get_search(self):
         self.log('Get properties for azure search {0}'.format(self.name))
         search_obj = None
-        account_dict = None
+        account_dict = list()
 
         try:
             search_obj = self.search_client.services.get(self.resource_group, self.name)
@@ -261,7 +261,7 @@ class AzureRMSearchInfo(AzureRMModuleBase):
             pass
 
         if search_obj:
-            account_dict = self.account_obj_to_dict(search_obj)
+            account_dict = [self.account_obj_to_dict(search_obj)]
 
         return account_dict
 
